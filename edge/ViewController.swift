a561b8825ed79b294dd22252122ec77088be8e77
Kenta Takano <kenta.takano@livesense.co.jp>
2017-08-24 13:36:22 +0900

    Initial Commit

---
+++ edge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  edge
+//
+//  Created by Kenta Takano on 2017/08/24.
+//  Copyright © 2017年 Kenta Takano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
